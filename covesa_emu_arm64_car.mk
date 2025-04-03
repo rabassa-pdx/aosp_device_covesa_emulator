2715a5b76671e220d9b7bd1cca13ac015eaf8343
rabassa-pdx <guillermo.rabassa@paradoxcat.com>
2025-04-03 14:56:54 +0200

    Update covesa_emu_arm64_car.mk
    Android 15 adds a new emulator_car64_x86_64 PRODUCT_DEVICE

--- covesa_emu_arm64_car.mk
+++ covesa_emu_arm64_car.mk
@@ -29,5 +29,5 @@ BUILD_EMULATOR_CLUSTER_DISPLAY := true
 $(call inherit-product, device/generic/goldfish/car/sdk_car_arm64.mk)
 
 PRODUCT_NAME := covesa_emu_arm64_car
-PRODUCT_DEVICE := emulator_arm64
+PRODUCT_DEVICE := emulator_car64_x86_64
 PRODUCT_MODEL := COVESA car emulator for arm64
