9274056b6905bc11d7b47082326594036d40672f
rabassa-pdx <guillermo.rabassa@paradoxcat.com>
2025-04-08 09:44:34 +0200

    Update covesa_emu_x86_64_car.mk
    Fixes inherit order in x86.

--- covesa_emu_x86_64_car.mk
+++ covesa_emu_x86_64_car.mk
@@ -21,8 +21,8 @@
 #
 
 
-$(call inherit-product, device/generic/goldfish/car/sdk_car_x86_64.mk)
 $(call inherit-product, device/covesa/common.mk)
+$(call inherit-product, device/generic/goldfish/car/sdk_car_x86_64.mk)
 
 PRODUCT_NAME := covesa_emu_x86_64_car
 PRODUCT_DEVICE := emulator_x86_64
