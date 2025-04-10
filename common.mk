91f2ca74f4f2389ea4da3dfd59022354ec2eb5c5
rabassa-pdx <guillermo.rabassa@paradoxcat.com>
2025-04-10 15:24:09 +0200

    Update common.mk
    Adds comments to common makefile

--- common.mk
+++ common.mk
@@ -8,8 +8,8 @@ BUILD_EMULATOR_CLUSTER_DISPLAY := true
 
 
 PRODUCT_PACKAGES += \
-	CustomCarProvision \
-	CustomCarLauncher
+	CustomCarProvision \ # Overrides CarProvision and ensures that the setup wizard is skipped
+	CustomCarLauncher # Small modification to the normal CarLauncher by making it directBootAware so that it's ready when provision finishes and doesn't show FallbackHome
 
 
 PRODUCT_CHARACTERISTICS := automotive
